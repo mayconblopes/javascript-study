c0753e3f5544f93b9d1c9f51257a88115d752dc7
Maycon Lopes <lopes.maycon@gmail.com>
2023-02-04 17:57:17 -0300

    Created GoodMornig Clock

---
+++ good_morning/script.js
@@ -0,0 +1,57 @@
+function Load(){
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('picture')
+    
+    let hr = document.querySelector('#hr')
+    let mn = document.querySelector('#mn')
+    let sc = document.querySelector('#sc')
+    
+    setInterval(() => {
+
+        let data = new Date()
+        let hours = data.getHours() * 30
+        let minutes = data.getMinutes() * 6
+        let seconds = data.getSeconds() * 6
+    
+        hr.style.transform = `rotateZ(${hours+(minutes/12)}deg)`
+        mn.style.transform = `rotateZ(${minutes}deg)`
+        sc.style.transform = `rotateZ(${seconds}deg)`
+
+        hours = hours / 30
+        minutes = minutes / 6
+        seconds = seconds / 6
+
+        msg.innerHTML = `<p>Agora são ${hours} : ${minutes} : ${seconds}</p>`
+        function setup(bgcolor, fontcolor) {
+            document.body.style.background = bgcolor
+            document.getElementById('title').style.color = fontcolor //change title text color to black
+            document.getElementById('footer').style.color = fontcolor // change footer text color to black
+    
+        }
+        
+        if (hours >= 0 && hours < 12) {
+            setup(bgcolor='#e0e0e0' ,fontcolor='#000')
+            document.getElementById('title').innerText = 'Good morning!'
+    
+        } else if (hours >= 12 && hours < 18) {
+            setup(bgcolor='#828e82' ,fontcolor='#fff')
+            document.getElementById('title').innerText = 'Good afternoon!'
+        } else {
+            setup(bgcolor='#3a606e' ,fontcolor='#fff')
+            document.getElementById('title').innerText = 'Good night!'
+        }
+
+    })
+
+    changeWallpaperButton = document.getElementById('new-wallpaper')
+    changeWallpaperButton.onclick = () => {
+        let randomId = parseInt(Math.random() * 201)
+        let url = `https://picsum.photos/id/${randomId}/300`
+       document.getElementById('clock').style.backgroundImage = `url(${url})`;
+    }
+
+
+
+
+
+}
